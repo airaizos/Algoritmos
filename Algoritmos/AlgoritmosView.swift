6608484056809735344dde4119cfe0762171a7e1
adrian <adrian@iraizos.me>
2021-07-26 19:13:17 +0200

    2. Añado Vista AlgoritmosView

---
+++ Algoritmos/AlgoritmosView.swift
@@ -0,0 +1,20 @@
+//
+//  AlgoritmosView.swift
+//  Algoritmos
+//
+//  Created by Adrian Iraizos Mendoza on 26/7/21.
+//
+
+import SwiftUI
+
+struct AlgoritmosView: View {
+    var body: some View {
+        Text("Mostrar Algoritmos")
+    }
+}
+
+struct AlgoritmosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlgoritmosView()
+    }
+}
